83e94433d4bb0d49af055a190ef14e718b28b487
corty2987 <cortyblackwell@gmail.com>
2023-04-30 18:50:31 -0500

    initial commit

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -1,13 +1,14 @@
 package org.launchcode.exercises;
 
 import java.util.HashMap;
-import java.util.Map;
+
+
 
 public class Main {
 
     public static void main(String[] args) {
-
-        // Test out your Divide() function!
+        Divide(1, 0);
+        Divide(15, 3);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -15,17 +16,41 @@ public class Main {
         studentFiles.put("Elizabeth", "MyCode.java");
         studentFiles.put("Stefanie", "CoolProgram.java");
 
-        // Test out your CheckFileExtension() function!
+        System.out.println(CheckFileExtension(studentFiles.get("Carl")));
+        System.out.println(CheckFileExtension(studentFiles.get("Brad")));
+        System.out.println(CheckFileExtension(studentFiles.get("Elizabeth")));
+        System.out.println(CheckFileExtension(studentFiles.get("Stefanie")));
     }
 
-    public static void Divide(int x, int y)
-    {
-        // Write code here!
+    public static void Divide(int x, int y) {
+        if (y == 0) {
+            try {
+                throw new ArithmeticException("Cannot divide by zero!");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        } else {
+            int result = x/y;
+            System.out.println(result);
+        }
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
-    }
+    public static int CheckFileExtension(String fileName) {
+        int numPoints;
+        int fileLength = fileName.length();
 
+        if (fileLength == 0) {
+            try {
+                throw new ArithmeticException("Missing file");
+            } catch (ArithmeticException e) {
+                numPoints = -1;
+                e.printStackTrace();
+            }
+        } else if (fileName.contains(".java")) {
+            numPoints = 1;
+        } else {
+            numPoints = 0;
+        }
+            return numPoints;
+    }
 }
